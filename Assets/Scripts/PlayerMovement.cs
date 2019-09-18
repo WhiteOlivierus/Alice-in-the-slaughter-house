9fb2e1257acf81b8da9f03caa9b5d2cd6bab663e
Bastien <bas.dijkstra@student.hku.nl>
2019-09-18 13:44:56 +0200

    Player movement working and implemented

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
-    {
+    public float speed;
 
+    private PlayerInput controls;
+    private Vector3 dir;
+
+    private void Awake()
+    {
+        controls = new PlayerInput();
     }
 
-    void Update()
+    private void OnEnable()
     {
+        controls.Player.Move.performed += ctx => Moving(ctx.ReadValue<Vector2>());
+        controls.Player.Move.canceled += ctx => Stopping();
+        controls.Player.Move.Enable();
+    }
 
+    private void OnDisable()
+    {
+        controls.Player.Move.performed -= ctx => Moving(ctx.ReadValue<Vector2>());
+        controls.Player.Move.canceled -= ctx => Stopping();
+        controls.Player.Move.Disable();
+    }
+
+    private void Moving(Vector2 direction)
+    {
+        dir = new Vector3(direction.x, 0f, direction.y);
+    }
+
+    private void Stopping()
+    {
+        dir = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        Move(dir);
+    }
+
+    private void Move(Vector3 direction)
+    {
+        transform.position += direction * speed * Time.fixedDeltaTime;
     }
 }
