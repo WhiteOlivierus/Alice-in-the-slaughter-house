fecba196a3ed10747719e71dbf12078019d88e0d
Bastien <bas.dijkstra@student.hku.nl>
2019-09-20 16:15:16 +0200

    finished pig carry controller

--- Assets/BlackBoard/BlackBoard.cs
+++ Assets/BlackBoard/BlackBoard.cs
@@ -3,6 +3,13 @@ using BB;
 
 public static class BlackBoard
 {
+    private static BlackBoardType<System.Single> _PigRange = new BlackBoardType<System.Single>(nameof(PigRange), typeof(PigCarry));
+    public static System.Single PigRange
+    {
+        get { return _PigRange.GetValue(); }
+        private set { }
+    }
+    
     private static BlackBoardType<System.Int32> _ButtonOffset = new BlackBoardType<System.Int32>(nameof(ButtonOffset), typeof(PigController));
     public static System.Int32 ButtonOffset
     {
